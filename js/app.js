eda0cb611c424cf8c845aba703633a572b08bfb5
shoxjaxon0905 <85515596+shoxjaxon0905@users.noreply.github.com>
2021-06-07 21:34:42 +0300

    AppLab

---
+++ js/app.js
@@ -0,0 +1,10 @@
+VANTA.BIRDS({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
